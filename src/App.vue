f2d48427012f7b7268f51721a484b47d27ea98b9
tangkaiqiang1 <justforgit@163.com>
2018-01-08 15:13:18 +0800

    router 动画

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- <transition :name="transitionName"> -->
+      <router-view class="child-view"/>
+    <!-- </transition> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
-}
+  name: "app",
+  data() {
+    return {
+      transitionName: "slide-left"
+    };
+  },
+  //监听路由的路径，可以通过不同的路径去选择不同的切换效果
+  watch: {
+    $route(to, from) {
+      if (to.path == "/") {
+        this.transitionName = "slide-right";
+      } else {
+        this.transitionName = "slide-left";
+      }
+    }
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+.child-view {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+.slide-left-enter,
+.slide-right-leave-active {
+  opacity: 0;
+  -webkit-transform: translate(30px, 0);
+  transform: translate(30px, 0);
+}
+.slide-left-leave-active,
+.slide-right-enter {
+  opacity: 0;
+  -webkit-transform: translate(-30px, 0);
+  transform: translate(-30px, 0);
+}
 </style>
